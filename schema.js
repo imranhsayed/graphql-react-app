347e65f36fbad23c435c6c984e238fe06e16f827
Imran Sayed <imranhsayed@gmail.com>
2019-05-11 19:05:44 +0530

    Add rocket type

--- schema.js
+++ schema.js
@@ -1,9 +1,29 @@
-const { GraphQLObjectType, GraphQLInt } = require( 'graphql' );
+const {
+	GraphQLObjectType,
+	GraphQLInt,
+	GraphQLString,
+	GraphQLBoolean
+} = require( 'graphql' );
+
+// RocketType
+const RocketType = new GraphQLObjectType({
+	name: 'Rocket',
+	fields: () => ({
+		rocket_id: { type: GraphQLString },
+		rocket_name: { type: GraphQLString },
+		rocket_type: { type: GraphQLString },
+	})
+});
 
 // Launch type
 const LaunchType = new GraphQLObjectType({
 	name: 'Launch',
 	fields: () => ({
 		flight_number: { type: GraphQLInt },
+		mission_name: { type: GraphQLString },
+		launch_year: { type: GraphQLString },
+		launch_date_local: { type: GraphQLString },
+		launch_success: { type: GraphQLBoolean },
+		rocket: { type: RocketType },
 	})
 });
