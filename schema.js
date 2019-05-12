dea722c9b16d2cdfb58920304e9f981037fa3762
Imran Sayed <imranhsayed@gmail.com>
2019-05-12 19:08:07 +0530

    Add rockets and rocket queries

--- schema.js
+++ schema.js
@@ -36,6 +36,7 @@ const LaunchType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
 	name: 'RootQueryType',
 	fields: {
+		// All Launches data
 		launches: {
 			type: new GraphQLList( LaunchType ),
 			resolve( parent, args ){
@@ -43,8 +44,28 @@ const RootQuery = new GraphQLObjectType({
 					.then( res => res.data );
 			}
 		},
+		// Single Launch data with id
 		launch: {
 			type: LaunchType,
+			args: {
+				flight_number: { type: GraphQLInt }
+			},
+			resolve( parent, args ) {
+				return axios.get( `https://api.spacexdata.com/v3/launches/${args.flight_number}` )
+					.then( res => res.data );
+			}
+		},
+		// All Rockets data
+		rockets: {
+			type: new GraphQLList( RocketType ),
+			resolve( parent, args ){
+				return axios.get( 'https://api.spacexdata.com/v3/rockets' )
+					.then( res => res.data );
+			}
+		},
+		// Single Rocket data with id
+		rocket: {
+			type: RocketType,
 			args: {
 				flight_number: { type: GraphQLInt }
 			},
